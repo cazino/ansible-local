95d2c319f1ea9c1766dacc53f3a9466aa1b50fd5
Emmanuel Cazenave <contact@emcaz.fr>
2016-03-30 10:10:45 +0200

    yaml mode for raml file

--- roles/emacs-config/files/init.el
+++ roles/emacs-config/files/init.el
@@ -33,6 +33,7 @@
   :config
   (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
   (add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
+  (add-to-list 'auto-mode-alist '("\\.raml$" . yaml-mode))
   (add-hook 'yaml-mode-hook
     '(lambda ()
        (define-key yaml-mode-map "\C-m" 'newline-and-indent)))
