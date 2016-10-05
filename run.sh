806eed1ec2d05ea8aec42dde7bc25642dc665dc9
Emmanuel Cazenave <contact@emcaz.fr>
2016-10-05 11:33:53 +0200

    Custom scripts and iventories

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-ansible-playbook -i hosts --ask-become-pass first.yml
+# USAGE : run.sh nameofinventory
+
+INVENTORY=$1
+
+ansible-playbook -i inventories/$INVENTORY --ask-become-pass first.yml
 . ~/.bashrc
-ansible-playbook -i hosts second.yml
+ansible-playbook -i inventories/$INVENTORY second.yml
 . ~/.bashrc
